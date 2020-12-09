4137d68ceb270a9099728c6588a0962fcf65404c
Kaligraphy247 <chikeziejames3@gmail.com>
2020-12-09 16:04:30 +0900

    💦 Added a text- Based Splash Screen

---
+++ riddle.py
@@ -0,0 +1,4 @@
+# Simple and interactive riddles written with python
+# back links here
+
+import splash
---
+++ splash.py
@@ -0,0 +1,30 @@
+import time, sys,os
+
+def typingPrint(text):
+    for character in text:
+        sys.stdout.write(character)
+        sys.stdout.flush()
+        time.sleep(0.03) # 0.5 for half a second... extra decimal places to make typePrint fast e.g 0.005
+
+def typingInput():
+    for character in text:
+        sys.stdout.write(character)
+        sys.stdout.flush()
+        time.sleep(0.05)
+    value = input()  
+    return value
+
+def clearScreen():
+    os.system("cls")
+
+typingPrint("#######################################\n")
+typingPrint("#######################################\n")
+typingPrint("###                                 ###\n")
+typingPrint("###      Welcome to Py_Riddle!      ###\n")
+typingPrint("###                                 ###\n")
+typingPrint("#######################################\n")
+typingPrint("#######################################\n")
+
+time.sleep(1)
+
+clearScreen()
