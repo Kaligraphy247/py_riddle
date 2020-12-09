28e2c955a8c9993f68e3b9db2f5c97b55a05f4cc
Kaligraphy247 <chikeziejames3@gmail.com>
2020-12-09 17:59:03 +0900

    🤔 Added 10 hard riddles

--- riddle.py
+++ riddle.py
@@ -63,9 +63,11 @@ answer05 =  str(input().lower())
 if answer05 == "age":
     typingPrint("Correct!\n")
 else:
-    typingPrint("Wrong, the right answer is 'age' \n Next...\n")
+    typingPrint("Wrong, the right answer is 'Age' \n Next...\n")
 
+typingPrint("Now, to hard Riddles.")
 clearScreen()
+
 
 typingPrint("1. What is so fragile that saying its name breaks it?\n")
 answer10 = str(input().lower())
@@ -73,3 +75,32 @@ if answer10 == "silence":
     typingPrint("Correct!\n")
 else:
     typingPrint("Wrong, the right answer is 'Silence' \n Next...\n")
+
+
+typingPrint("2. What can run but never walks, has a mouth but never talks, has a head but never weeps, has a bed but never sleeps?\n")
+answer11 = 
+
+typingPrint("3. What can fill a room but takes up no space?\n")
+
+typingPrint("4. If you drop me I’m sure to crack, but give me a smile and I’ll always smile back. What am I?\n")
+
+typingPrint("5. The more you take, the more you leave behind. What are they?\n")
+
+typingPrint("6. I turn once, what is out will not get in. I turn again, what is in will not get out. What am I?\n")
+
+typingPrint("7.  People make me, save me, change me, raise me. What am I?\n")
+
+typingPrint("8. I am always hungry and will die if not fed, but whatever I touch will soon turn red. What am I?\n")
+
+typingPrint("9. What goes through cities and fields, but never moves?\n")
+
+typingPrint("10. With pointed fangs I sit and wait; with piercing force I crunch out fate; grabbing victims, proclaiming might; physically joining with a single bite. What am I?\n")
+
+clearScreen()
+typingPrint("Some Bonus Riddle, you may terminate now if you wish to stop")
+print("You may terminate now if you wish to stop")
+clearScreen()
+
+typingPrint("a.  I have lakes with no water, mountains with no stone and cities with no buildings. What am I?\n")
+
+typingPrint("b. What does man love more than life, hate more than death or mortal strife; that which contented men desire; the poor have, the rich require; the miser spends, the spendthrift saves, and all men carry to their graves?\n")
