6322768a288813703597eb2ca9cc953f755fabe7
Kaligraphy247 <chikeziejames3@gmail.com>
2022-02-17 19:48:31 +0100

    All done

--- bye.py
+++ bye.py
@@ -13,15 +13,16 @@ def clearScreen():
         os.system("cls")
     elif os.name == 'posix':
         os.system("clear")
-
-typingPrint(Fore.MAGENTA + "#######################################\n") # THIS IS SUPPOSED TO REFLECT PURPLE 😅
-typingPrint(Fore.BLUE + "#######################################\n") # THIS IS SUPPOSED TO REFLECT INDIGO 😅
-typingPrint(Fore.LIGHTBLUE_EX + "###                                 ###\n")
-typingPrint(Fore.GREEN + "###              Bye!!!             ###\n")
-typingPrint(Fore.LIGHTYELLOW_EX + "###                                 ###\n")
-typingPrint(Fore.YELLOW + "#######################################\n") # THIS IS SUPPOSED TO REFLECT ORANGE 😅
-typingPrint(Fore.RED + "#######################################\n") 
-print(Fore.RESET) # so your terminal doesn't retain the last color printed...🤔
-time.sleep(2)
+def bye():
+    '''Bye splash screen'''
+    typingPrint(Fore.MAGENTA + "#######################################\n") # THIS IS SUPPOSED TO REFLECT PURPLE 😅
+    typingPrint(Fore.BLUE + "#######################################\n") # THIS IS SUPPOSED TO REFLECT INDIGO 😅
+    typingPrint(Fore.LIGHTBLUE_EX + "###                                 ###\n")
+    typingPrint(Fore.GREEN + "###              Bye!!!             ###\n")
+    typingPrint(Fore.LIGHTYELLOW_EX + "###                                 ###\n")
+    typingPrint(Fore.YELLOW + "#######################################\n") # THIS IS SUPPOSED TO REFLECT ORANGE 😅
+    typingPrint(Fore.RED + "#######################################\n") 
+    print(Fore.RESET) # so your terminal doesn't retain the last color printed...🤔
+    time.sleep(2)
 
 clearScreen()
--- splash.py
+++ splash.py
@@ -13,16 +13,15 @@ def clearScreen():
         os.system("cls")
     elif os.name == 'posix':
         os.system("clear")
-
-typingPrint(Fore.RED + "#######################################\n")
-typingPrint(Fore.YELLOW + "#######################################\n")
-typingPrint(Fore.LIGHTYELLOW_EX + "###                                 ###\n")
-typingPrint(Fore.GREEN + "###      Welcome to Py_Riddle!      ###\n")
-typingPrint(Fore.LIGHTBLUE_EX + "###                                 ###\n")
-typingPrint(Fore.BLUE + "#######################################\n")
-typingPrint(Fore.MAGENTA + "#######################################\n")
-
-print(Fore.RESET)
-
-time.sleep(2)
-clearScreen()
+def splash():
+    '''Splash Screen '''
+    typingPrint(Fore.RED + "#######################################\n")
+    typingPrint(Fore.YELLOW + "#######################################\n")
+    typingPrint(Fore.LIGHTYELLOW_EX + "###                                 ###\n")
+    typingPrint(Fore.GREEN + "###      Welcome to Py_Riddle!      ###\n")
+    typingPrint(Fore.LIGHTBLUE_EX + "###                                 ###\n")
+    typingPrint(Fore.BLUE + "#######################################\n")
+    typingPrint(Fore.MAGENTA + "#######################################\n")
+    print(Fore.RESET)
+    time.sleep(2)
+    clearScreen()
